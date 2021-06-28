70f0288159efd60f8bd0cf6143073451756520ef
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-06-28 12:18:54 +0700

    edit controller and route token.js

--- controllers/tokens.js
+++ controllers/tokens.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt-nodejs');
+const ModelTokens = require('../models').Tokens;
+const ModelClients = require('../models').Clients;
 const ModelUsers = require('../models').Users;
 
 exports.register = function (req, res) {
@@ -16,6 +19,111 @@ exports.register = function (req, res) {
         });
 };
 
-exports.login = function (req, res) {
-    
+module.exports.getAccessToken = function (bearerToken) {
+    return ModelTokens.findOne(
+        {
+            where: {
+                accessToken: bearerToken
+            },
+            include: [
+                {
+                    model: ModelClients,
+                    as: 'client'
+                },
+                {
+                    model: ModelUsers,
+                    as: 'user'
+                }
+            ]
+        })
+        .then((token) => {
+            const data = new Object();
+            for (const prop in token.get()) data[prop] = token[prop];
+            data.client = data.client.get();
+            data.user = data.user.get();
+            return data;
+        })
+        .catch((error) => console.error(error));
+};
+
+module.exports.getClient = function (clientId, clientSecret) {
+    return ModelClients.findOne({where: {clientId: clientId, clientSecret: clientSecret}, raw: true});
+};
+
+module.exports.getRefreshToken = function (refreshToken) {
+    return ModelTokens.findOne(
+        {
+            where: {
+                refreshToken: refreshToken
+            },
+            include: [
+                {
+                    model: ModelClients,
+                    as: 'client'
+                },
+                {
+                    model: ModelUsers,
+                    as: 'user'
+                }
+            ]
+        })
+        .then((token) => {
+            const data = new Object();
+            for (const prop in token.get()) data[prop] = token[prop];
+            data.client = data.client.get();
+            data.user = data.user.get();
+            return data;
+        })
+        .catch((error) => console.error(error));
+};
+
+module.exports.getUser = function (username, password) {
+    return ModelUsers.findOne({where: {username: username}})
+        .then((user) => {
+            const isMatch = bcrypt.compareSync(password, user.get().password);
+            if (isMatch) {
+                return user.get();
+            } else {
+                console.error("Password not match");
+            }
+        });
+};
+
+module.exports.saveToken = function (token, client, user) {
+    return ModelTokens
+        .create(
+            {
+                accessToken: token.accessToken,
+                accessTokenExpiresAt: token.accessTokenExpiresAt,
+                clientId: client.id,
+                refreshToken: token.refreshToken,
+                refreshTokenExpiresAt: token.refreshTokenExpiresAt,
+                userId: user.id
+            }
+        )
+        .then((token) => {
+            const data = new Object();
+            for (const prop in token.get()) data[prop] = token[prop];
+            data.client = data.clientId;
+            data.user = data.userId;
+
+            return data;
+        })
+        .catch((error) => console.error(error));
+};
+
+module.exports.revokeToken = function (token) {
+    console.log("Revoke token");
+    return ModelTokens
+        .findOne({where: {refreshToken: token.refreshToken}})
+        .then(refreshToken => {
+            console.log(refreshToken);
+            return refreshToken
+                .destroy()
+                .then(() => {
+                    return !!refreshToken
+                })
+                .catch((error) => console.error(error));
+        })
+        .catch((error) => console.error(error));
 };
--- routes/tokens.js
+++ routes/tokens.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/tokens');
+const OAuthServer = require('express-oauth-server');
+
+router.oauth = new OAuthServer({
+    model: controller
+});
 
 router.post('/v1/register', controller.register);
-router.post('/v1/login', controller.login);
+router.post('/v1/login', router.oauth.token());
 
 module.exports = router;
